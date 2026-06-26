1c0f2f20e959f11d3c92817f04120ab7c97e28c9
SBTechLab <sbbhalani11@gmail.com>
2026-06-26 13:05:47 +0530

    09 theme changer

---
+++ 09theamechanger/src/Context/Theame.js
@@ -0,0 +1,13 @@
+import { createContext, useContext } from "react";
+
+export const TheameContext = createContext({
+    themeMode: "light",
+    darkTheme: () => {},
+    lightTheme: () => {},
+})
+
+export const ThemeProvider = TheameContext.Provider
+
+export default function useTheme() {
+    return useContext(TheameContext)
+}
