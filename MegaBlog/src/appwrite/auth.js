226aa121eb202a6855393c34c065142c127ad63e
SBTechLab <sbbhalani11@gmail.com>
2026-08-02 14:49:17 +0530

    add services in auth, appwrite's 1 service add successful

---
+++ MegaBlog/src/appwrite/auth.js
@@ -0,0 +1,95 @@
+import conf from '../conf/conf.js';
+import {Client, Account,Id} from "appwrite";    
+
+
+
+
+export class AuthService {
+    client = new Client();
+    account;
+
+    
+    
+    
+    constructor() {
+        this.client 
+              .setEndpoint(conf.appwriteurl)
+              .setProject(conf.appwriteProjectId);
+            this.account = new Account(this.client);
+        }
+
+
+
+
+
+    async createAccount ({email, password, name}) {
+        try{
+            const userAccount = await this.account.create
+              (ID.unique(),email,password,name);
+              
+              if (userAccount) {
+                //call another method
+                return this.login({email,password});
+
+              } else {
+                 return userAccount;
+              }
+              
+        }catch (error) {
+            throw error;
+        }
+}
+
+
+async login ({email,password}) {
+
+    try {
+       return  await this.account.createEmailSession(email,password);
+
+    } catch (error) {
+        throw error;
+    }
+
+
+}
+
+
+
+
+
+ async getCurrentUser() {
+
+    try{
+
+    } catch (error) {
+        console.log("Appwrite service :: getCurremtUser :: error", error);
+    }
+
+    return null;
+
+ }
+
+
+
+
+
+ async logout() {
+    try {  
+          await this.account.deleteSession();
+
+    }catch (error) {
+        console.log("Appwrite service :: logout :: error", error);
+
+    }
+ }
+
+
+
+
+
+}
+
+const authService = new AuthService();
+
+export default AuthService
+
